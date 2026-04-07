97b5f6003b8a841ac8d585960825d85a5c46675b
carol <mosnguyen@ucdavis.edu>
2026-04-07 02:04:53 -0700

    portal done 1

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 (function() {
     'use strict';
+    console.log('reading js');
 
     const button = document.querySelector('button');
     const body = document.querySelector('body');
     const banner = document.querySelector('#banner');
-    const sections = document.querySelectorAll('section')
-    let mode = 'dark';
+    const sections = document.querySelectorAll('section');
+    let mode = 'off';
 
     button.addEventListener('click', function() {
-        if (mode === 'dark') {
+        if (mode === 'off') {
             body.className = 'switch';
             banner.className = 'switch';
             button.className = 'switch';
             for (const section of sections) {
                 section.className = 'switch';
             }
-            mode = 'light';
+            mode = 'on';
         } else {
             body.removeAttribute('class');
             banner.removeAttribute('class');
@@ -23,7 +24,8 @@
             for (const section of sections) {
                 section.removeAttribute('class');
             }
-            mode = 'dark'
+            mode = 'off'
         }
     })
+
 })()
