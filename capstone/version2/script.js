0d892f02db24046143200a24da4ecc18d7e80ccf
mos-nguyen <mosnguyen@ucdavis.edu>
2026-05-26 15:40:51 -0700

    getting there

--- capstone/version2/script.js
+++ capstone/version2/script.js
@@ -10,7 +10,7 @@
         overlay.className='done';
         // text.className='open';
     });
-
+// ----------------------------------------------------------------------------------------
     const envelope = document.querySelector('.fa-envelope');
     const portal = document.querySelector('.fa-door-closed');
     const openInventory = document.querySelector('.fa-briefcase');
@@ -29,23 +29,23 @@
         // console.log('werk');
         inventory.classList.remove('.done')
         inventory.className='open';
-        box.className='done';
     });
-
+// ----------------------------------------------------------------------------------------
     const inventory = document.querySelector('#inventory');
     const close = document.querySelector('#inventory p')
 
     close.addEventListener('click', function() {
         inventory.classList.remove('.open')
         inventory.className='done';
-        box.classList.remove('.done');
-        box.className='open';
 
     })
-
+// ----------------------------------------------------------------------------------------
     const next = document.querySelector('#next');
     const box = document.querySelector('#box')
     const boxText  = document.querySelector('#box p');
+
+    let textIndex = 0;
+
     const textBank = {
         newText: [
             'does this work?',
@@ -56,7 +56,13 @@
 
     next.addEventListener('click', function() {
         console.log('testing');
-        box.innerHTML = `<p>${textBank.newText[0]}</p>`
+        if (textIndex < textBank.newText.length) {
+            boxText.innerHTML = `<p>${textBank.newText[textIndex]}</p>`;
+        textIndex ++
+        } else {
+            next.style.display='none';
+        }
+        
     }) 
 
 })();
